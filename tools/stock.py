18eecaaeb845e36e19d5308d89f11e79c28fb5da
Justin Xiao <o927416847@gmail.com>
2023-08-15 00:35:34 +0800

    feat: add stock tool

---
+++ tools/stock.py
@@ -0,0 +1,70 @@
+from typing import Type
+from pydantic import BaseModel, Field
+from langchain.tools import BaseTool
+import yfinance as yf
+from datetime import datetime, timedelta
+
+
+def get_current_stock_price(ticker):
+    """Method to get current stock price"""
+
+    ticker_data = yf.Ticker(ticker)
+    recent = ticker_data.history(period="1d")
+    return {"price": recent.iloc[0]["Close"], "currency": ticker_data.info["currency"]}
+
+
+def get_stock_performance(ticker, days):
+    """Method to get stock price change in percentage"""
+
+    past_date = datetime.today() - timedelta(days=days)
+    ticker_data = yf.Ticker(ticker)
+    history = ticker_data.history(start=past_date)
+    old_price = history.iloc[0]["Close"]
+    current_price = history.iloc[-1]["Close"]
+    return {"percent_change": ((current_price - old_price) / old_price) * 100}
+
+class CurrentStockPriceInput(BaseModel):
+    """Inputs for get_current_stock_price"""
+
+    ticker: str = Field(description="Ticker symbol of the stock")
+
+
+class CurrentStockPriceTool(BaseTool):
+    name = "get_current_stock_price"
+    description = """
+        Useful when you want to get current stock price.
+        You should enter the stock ticker symbol recognized by the yahoo finance
+        """
+    args_schema: Type[BaseModel] = CurrentStockPriceInput
+
+    def _run(self, ticker: str):
+        price_response = get_current_stock_price(ticker)
+        return price_response
+
+    def _arun(self, ticker: str):
+        raise NotImplementedError("get_current_stock_price does not support async")
+
+
+class StockPercentChangeInput(BaseModel):
+    """Inputs for get_stock_performance"""
+
+    ticker: str = Field(description="Ticker symbol of the stock")
+    days: int = Field(description="Timedelta days to get past date from current date")
+
+
+class StockPerformanceTool(BaseTool):
+    name = "get_stock_performance"
+    description = """
+        Useful when you want to check performance of the stock.
+        You should enter the stock ticker symbol recognized by the yahoo finance.
+        You should enter days as number of days from today from which performance needs to be check.
+        output will be the change in the stock price represented as a percentage.
+        """
+    args_schema: Type[BaseModel] = StockPercentChangeInput
+
+    def _run(self, ticker: str, days: int):
+        response = get_stock_performance(ticker, days)
+        return response
+
+    def _arun(self, ticker: str):
+        raise NotImplementedError("get_stock_performance does not support async")
