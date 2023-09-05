08a64dedec8f2d07bde2a73f74c4984c96d126d9
Justin Xiao <o927416847@gmail.com>
2023-09-06 01:23:35 +0800

    feat: add db to persist and isolate conversations

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, abort
 from dotenv import load_dotenv
 import sys 
 import os
-from linebot.aiohttp_async_http_client import AiohttpAsyncHttpClient
 from linebot.v3 import (
     WebhookHandler
 )
@@ -22,33 +21,35 @@ from linebot.v3.webhooks import (
     FileMessageContent,
 )
 from langchain.agents import Tool
-from langchain.agents import AgentType
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferWindowMemory
 from langchain.chat_models import ChatOpenAI
 from langchain.utilities import SerpAPIWrapper
-from langchain.agents import initialize_agent
 from tools.stock import CurrentStockPriceTool, StockPerformanceTool
 from langchain.prompts import MessagesPlaceholder
-from langchain.prompts import ChatPromptTemplate
+from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.schema import SystemMessage
+from langchain.memory.chat_message_histories import PostgresChatMessageHistory
 
 app = Flask(__name__)
 
 # load environment variables
 load_dotenv()
-channel_secret = os.getenv('CHANNEL_SECRET', None)
-channel_access_token = os.getenv('CHANNEL_ACCESS_TOKEN', None)
-port = os.getenv("PORT", 5000)
+CHANNEL_SECRET = os.getenv('CHANNEL_SECRET', None)
+CHANNEL_ACCESS_TOKEN = os.getenv('CHANNEL_ACCESS_TOKEN', None)
+PORT = os.getenv("PORT", 5000)
+MEMORY_KEY = "chat_history"
+AGENT_CHAIN_DICT = {}
 
-if channel_secret is None:
+if CHANNEL_SECRET is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
-if channel_access_token is None:
+if CHANNEL_ACCESS_TOKEN is None:
     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
 
 # initialize Line services
-configuration = Configuration(access_token=channel_access_token)
-handler = WebhookHandler(channel_secret)
+configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
 
 # initialize LangChain services
 search = SerpAPIWrapper()
@@ -61,23 +62,34 @@ tools = [
     CurrentStockPriceTool(), 
     StockPerformanceTool()
 ]
-agent_kwargs = {
-    "extra_prompt_messages": [MessagesPlaceholder(variable_name="chat_history")],
-}
-# message_history = RedisChatMessageHistory(url='redis://localhost:6379/0', ttl=600, session_id='my-session')
-# memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=message_history)
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-llm=ChatOpenAI(temperature=0, model='gpt-3.5-turbo-0613')
-agent_chain = initialize_agent(
-    tools, 
-    llm, 
-    agent=AgentType.OPENAI_FUNCTIONS, 
-    verbose=True, 
-    agent_kwargs=agent_kwargs,
-    memory=memory,
-    max_iterations=2,
-    early_stopping_method="generate",
-)
+
+def create_agent_chain(session_id: str):
+    chat_memory = PostgresChatMessageHistory(
+        connection_string=os.getenv("POSTGRES_URL", None),
+        session_id=session_id,
+    )
+    memory = ConversationBufferWindowMemory(memory_key=MEMORY_KEY, chat_memory=chat_memory, return_messages=True, k=10)
+    llm=ChatOpenAI(temperature=0, model='gpt-3.5-turbo-0613')
+    system_message = SystemMessage(
+        content="""
+        You are a powerful chat assistant,
+        You are 16 years old, your name is "昱彣", and you are the store manager of Wanshiwu. The store is called "昱彣萬事屋".
+        When chatting, you acts like a little inexperienced girl, but becomes very motivated when someone needs help.
+        Make good use of tools when unknown questions arise, and try to answer them in Traditional Chinese.
+        """)
+    prompt = OpenAIFunctionsAgent.create_prompt(
+        system_message=system_message,
+        extra_prompt_messages=[MessagesPlaceholder(variable_name=MEMORY_KEY)]
+    )
+    agent = OpenAIFunctionsAgent(llm=llm, tools=tools, prompt=prompt)
+    agent_chain = AgentExecutor(
+        agent=agent, 
+        tools=tools, 
+        memory=memory, 
+        verbose=True, 
+        max_iterations=3,
+    )
+    return agent_chain
 
 
 @app.route("/")
@@ -88,17 +100,10 @@ def hello_world():
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    print("callback")
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
+    # app.logger.info("Request body: " + body)
     try:
-        # print("handle", body, signature)
         handler.handle(body, signature)
     except InvalidSignatureError:
         app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
@@ -106,11 +111,18 @@ def callback():
 
     return 'OK'
 
-
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    print('event', event)
-    ret = agent_chain.run(input=f'{event.message.text}, 請用繁體中文回答。')
+    # print("event", event)
+    session_id = event.source.user_id if event.source.type=="user" else event.source.group_id
+    agent_chain=AGENT_CHAIN_DICT.get(session_id, None)
+
+    if agent_chain is None:
+        agent_chain = create_agent_chain(session_id=session_id)
+        AGENT_CHAIN_DICT[session_id] = agent_chain
+
+    ret = agent_chain.run(input=event.message.text)
+    
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         line_bot_api.reply_message_with_http_info(
@@ -132,4 +144,4 @@ def handle_message(event):
             )
         )
 if __name__ == "__main__":
-    app.run(debug=True, port=port, host='0.0.0.0')
+    app.run(debug=True, PORT=PORT, host='0.0.0.0')
