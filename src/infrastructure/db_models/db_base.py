f40cb85f36a74d7b491e51a48bcfaf851ca10f52
Justin Xiao <o927416847@gmail.com>
2023-09-20 03:56:18 +0800

    refactor: change the strategy of connecting to database

--- src/infrastructure/db_models/db_base.py
+++ src/infrastructure/db_models/db_base.py
@@ -1,12 +1,11 @@
-""" Base class for all models
-"""
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, scoped_session
+from sqlalchemy import create_engine
 
-
-from sqlalchemy.orm import DeclarativeBase
-
+from src.app.flask_postgresql.configs import Config
 
 class Base(DeclarativeBase):
     """ Base class for all models
     """
 
-
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
+Session = scoped_session(sessionmaker(bind=engine))
